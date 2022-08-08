2cd6d1f59c3cded1ffabccb2a0ad54106b365cba
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-08-07 21:36:10 -0500

    updated my About and Navigation; need to fix my navigation header

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
-// import coverImage from './assets/anjuli.jpg';
+import coverImage from '../../assets/anjuli.jpg';
 
 
 function About() {
 
   return (
-    <section className="my-5">
-    <h1 id="about"> Early WOC Front-End Developer,Social Impact Driven,Dog Enthusiast</h1>
-    <img id= "img" src="" className="my-2" style={{ width: "50%" }} alt="cover" />
-    <div className="my-2 bio">
+    <div className="container text-center my-5">
+      <div className="row align-items-center">
+        <div className="col-6 mx-3">
+        <h2 className="about"id="about"> WOC Front-End Developer+Social Impact Driven+Dog Enthusiast</h2>
+        <img id= "img" src={coverImage} className="my-2" style={{ width: "50%" }} alt="cover" />
+        </div>
+    <div className="bio col-4 mx-3" id="bio">
       <p>
         Hello — My name is Anjuli Garcia. I am an aspiring software developer, with a preference in front-end development and design, and a knack for helping purpose-driven businesses. I would like to dedicate my skills to companies who have a passion for helping people and the planet. I've combine my passions for social impact, code and creativity into my current freelance hobby, Studio Junk. I believe championing impact driven brands will level up our communities and our planet. In the meantime, I enjoy going to the movies, hanging with my dog Elton John, and eating good ole saucy pizza.
         If my aspirations and skills resonate with you, feel free to contact me. I hope you liked the preview!
       </p>
     </div>
-  </section>
+  </div>
+  </div>
   );
 }
 
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 function Nav() {
   
     return (
-        <header className="flex-row px-1 container" id="header">
-            <h2>
+        <header className="d-flex flex-row container position-fixed top-0" id="header">
+            <h2 id="logo">
                 <a href="/">
                     ANJULI GARCIA
                 </a>
             </h2>
-            <nav>
-                <ul className="container nav" id="nav">
+            <nav className="mx-1 container ">
+                <ul className=" nav " id="nav">
                     <li>
                         <a href="#about">
                         ABOUT
